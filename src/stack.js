1189624d5863f7f058c387860c284ad9c4fcab42
Mariya Demy <Mariya.Demi373@gmail.com>
2023-05-26 17:05:02 +0300

    refactor: make push and pop methods more productive

--- src/stack.js
+++ src/stack.js
@@ -9,28 +9,29 @@ class Stack {
     this.stackArr = [];
   }
   push(elem) {
-    this.stackArr.unshift(elem);
+    this.stackArr.push(elem);
   }
 
   pop() {
-    return this.stackArr.shift(this.stackArr[0]);
+    return this.stackArr.pop();
   }
 
   peek() {
-    return this.stackArr[0];
+    return this.stackArr[this.stackArr.length - 1];
   }
 }
 
 // @example
-/*
+
 const stack = new Stack();
+console.log(stack);
+
 stack.push(1); // adds the element to the stack
 stack.push(5);
-stack.peek(); // returns the peek, but doesn't delete it, returns 1
-stack.pop(); // returns the top element from stack and deletes it, returns 1
-stack.pop(); // undefined
-console.log(stack)
-*/
+console.log(stack.peek(), 'stack peek'); // returns the peek, but doesn't delete it, returns 5
+stack.pop(); // returns the top element from stack and deletes it, returns 5
+console.log(stack.pop()); // 1
+console.log(stack.pop()); // undefined
 
 module.exports = {
  Stack
